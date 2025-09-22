d3b6f9ddde477cd146a9887a022e5ef0af1666ea
hapuum <lbklbk9345@gmail.com>
2025-09-22 06:57:48 -0700

    add: fully functioning tree generation

--- json_parser.cu
+++ json_parser.cu
@@ -4,53 +4,57 @@
 #include <vector>
 
 using namespace json;
+using namespace std;
 
-json_object::json_object() {
-    data = std::map<std::string, payload>();
+namespace json {
+    json_object::json_object() {
+        data = std::map<std::string, payload>();
+    }
+
+    json_object::~json_object() {
+    }
+
+    void json_object::addObject(std::string key, int* val) {
+        data[key] = payload(val, INTEGER);
+    }
+
+    void json_object::addObject(std::string key, double* val) {
+        data[key] = payload(val, DOUBLE);
+    }
+
+    void json_object::addObject(std::string key, std::string* val) {
+        data[key] = payload(val, STRING);
+    }
+
+    void json_object::addObject(std::string key, bool* val) {
+        data[key] = payload(val, BOOLEAN);
+    }
+
+    void json_object::addObject(std::string key, json_object* val) {
+        data[key] = payload(val, JSON);
+    }
+
+    void json_object::addObject(std::string key, std::vector<payload>* val) {
+        data[key] = payload(val, LIST);
+    }
+
+    payload json_object::operator[](std::string key) {
+        return data[key];
+    }
+
+    void task::setStart(int s) {
+        start = s;
+    }
+
+    void task::setEnd(int e) {
+        end = e;
+    }
+
+    void task:: setObject(json_object* o) {
+        obj = o;
+    }
 }
 
-json_object::~json_object() {
-}
-
-void json_object::addObject(std::string key, int* val) {
-    data[key] = payload(val, INTEGER);
-}
-
-void json_object::addObject(std::string key, double* val) {
-    data[key] = payload(val, DOUBLE);
-}
-
-void json_object::addObject(std::string key, std::string* val) {
-    data[key] = payload(val, STRING);
-}
-
-void json_object::addObject(std::string key, bool* val) {
-    data[key] = payload(val, BOOLEAN);
-}
-
-void json_object::addObject(std::string key, json_object* val) {
-    data[key] = payload(val, JSON);
-}
-
-void json_object::addObject(std::string key, std::vector<payload>* val) {
-    data[key] = payload(val, LIST);
-}
-
-payload json_object::operator[](std::string key) {
-    return data[key];
-}
-
-void task::setStart(int s) {
-    start = s;
-}
-
-void task::setEnd(int e) {
-    end = e;
-}
-
-void task:: setObject(json_object* o) {
-    obj = o;
-}
 
 // debug purpose
 std::ostream& operator<< (std::ostream& stream, const json_object& json);
@@ -91,7 +95,6 @@ std::ostream& operator<< (std::ostream& stream, const payload p) {
     }
     return stream;
 }
-
 std::ostream& operator<< (std::ostream& stream, const json_object& json) {
     stream << "{";
     bool first = true;
@@ -107,51 +110,13 @@ std::ostream& operator<< (std::ostream& stream, const json_object& json) {
     return stream;
 }
 
-int main() {
-    using namespace std;
-    // TEST CLASS STRUCTURE
-    json_object json1, json2, json3, json4;
-    vector<payload> collection;
-    
-    int i = 5;
-    bool b = true;
-    json1.addObject("first", &i);
-    json1.addObject("second", &b);
-    string s = "example string";
-    json2.addObject("third", &s);
-
-    payload p1(&i, INTEGER);
-    payload p2(&json1, JSON);
-    collection.push_back(p1);
-    collection.push_back(p2);
-    json3.addObject("fourth", &collection);
-    json4.addObject("fifth", &json2);
-    cout << json1 << endl;
-    cout << json2 << endl;
-    cout << json3 << endl;
-    cout << json4 << endl;
-
-    bool testing = false;
-    string filepath = (!testing) ? "/home/hapuum/cuda_learn/resource/relic_data.json" : "/home/hapuum/cuda_learn/resource/test.json";
-
-    ifstream file(filepath);
-    if (!file.is_open()) 
-    {
-        std::cerr << "Error opening file: " << filepath << std::endl;
-        return EXIT_FAILURE;
-    }
-    string json_content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-    file.close();
-    cout << "File loaded successfully. Size: " << json_content.size() << " bytes" << endl;
-
-    // CPU sequentially traverses json content char[] and finds pairs using stack, stores it in Task[]
-    // later to be replaced with a parellel algorithm (up sweep / down sweep based prefix sum pair generation)
-    // some efforts for parallelizing this part is in cuda/cuda_hsr_sim.cu, but needs more work for now.
-    
-    
-    // string (variable name) also needs a stack of its own it seems.
+// CPU sequentially traverses json content char[] and finds pairs using stack, stores it in Task[]
+// later to be replaced with a parellel algorithm (up sweep / down sweep based prefix sum pair generation)
+// some efforts for parallelizing this part is in cuda/cuda_hsr_sim.cu, but needs more work for now.
+void create_json_tree(const std::string& json_content, std::vector<task*>& task_dispatchable) {
     stack<task*> task_stack;
-    vector<task*> task_dispatchable;
+    stack<bool> inList_stack;
+    stack<vector<payload>*> current_list_stack;
 
     bool inString = false;
     bool inList = false;
@@ -197,34 +162,72 @@ int main() {
                 break;
             }
             case '[' : {  // need better logic when adding nested array support
-                inList = !inList;
+                inList_stack.push(inList);
+                inList = true;
+                current_list_stack.push(current_list);
                 current_list = new vector<payload>();
                 current_json->addObject(current_string, current_list);
+                cout << current_string << endl;
                 break;
             }
             case ']' : {
-                inList = !inList;
+                inList = inList_stack.top();
+                inList_stack.pop();
+                current_list = current_list_stack.top();
+                current_list_stack.pop();
                 break;
             }
-            case ',' : {
-                if (inList && !inString) {
-                    // push it to correct spot?? dont really need if we dont care about serialization.
-                    // also the algorithm feels awkward on this... needs a separate stack to maintain if this is in a list or not
-                }
+            default:
                 break;
-            }
         }
-    }    
+    }  
+}
+
+void test_class_structure() {
+    json_object json1, json2, json3, json4;
+    vector<payload> collection;
+    
+    int i = 5;
+    bool b = true;
+    json1.addObject("first", &i);
+    json1.addObject("second", &b);
+    string s = "example string";
+    json2.addObject("third", &s);
+
+    payload p1(&i, INTEGER);
+    payload p2(&json1, JSON);
+    collection.push_back(p1);
+    collection.push_back(p2);
+    json3.addObject("fourth", &collection);
+    json4.addObject("fifth", &json2);
+    cout << json1 << endl;
+    cout << json2 << endl;
+    cout << json3 << endl;
+    cout << json4 << endl;
+}
+
+int main() {
+    test_class_structure();
+
+    bool testing = false;
+    string filepath = (!testing) ? "/home/hapuum/cuda_learn/resource/relic_data.json" : "/home/hapuum/cuda_learn/resource/test.json";
+
+    ifstream file(filepath);
+    if (!file.is_open()) 
+    {
+        std::cerr << "Error opening file: " << filepath << std::endl;
+        return EXIT_FAILURE;
+    }
+    string json_content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    file.close();
+    cout << "File loaded successfully. Size: " << json_content.size() << " bytes" << endl;
+    
+    vector<task*> task_dispatchable;
+    create_json_tree(json_content, task_dispatchable);
 
     // debug + prep for gpu
     for (int i = 0; i < task_dispatchable.size(); i++) {
         task* t = task_dispatchable[i];
-        cout << "start :" << t->start << ", end:" << t->end << " json :" << *(t->obj) << " at memory: " << t->obj << endl;
+        cout << "start :" << t->start << ", end:" << t->end << " json: " << *(t->obj) << " at memory: " << t->obj << endl;
     }
-
-    
-    // GPU parallel processes all of the task_dispatchable and fills in the json_object in each task
-    // extract them for host use
-
-
 }
