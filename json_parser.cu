c9636634a629b004e07b0a6a42fcd4904c1e3f3e
hapuum <lbklbk9345@gmail.com>
2025-09-21 17:02:25 -0700

    update list logic and debug logic

--- json_parser.cu
+++ json_parser.cu
@@ -1,4 +1,7 @@
 #include "json_parser.h"
+#include <fstream>
+#include <stack>
+#include <vector>
 
 using namespace json;
 
@@ -44,9 +47,9 @@ void json_object::addObject(std::string key, json_object* val) {
     data[key] = p;
 }
 
-void json_object::addObject(std::string key, std::vector<json_object>* val) {
+void json_object::addObject(std::string key, std::vector<payload>* val) {
     payload p;
-    p.type = JSON_LIST;
+    p.type = LIST;
     p.value = val;
     data[key] = p;
 }
@@ -55,15 +58,78 @@ payload json_object::operator[](std::string key) {
     return data[key];
 }
 
-int main() {
-    // TESTING CLASS STRUCTURE -- SUBJECT TO CHANGE AS WE MAKE IT DEVICE COMPATIBLE
-    // STD::MAP and STD::VECTOR needs replacement
-    //
+void task::setStart(int s) {
+    start = s;
+}
 
-    
-    // using namespace std;
+void task::setEnd(int e) {
+    end = e;
+}
+
+void task:: setObject(json_object* o) {
+    obj = o;
+}
+
+// debug purpose
+std::ostream& operator<< (std::ostream& stream, const json_object& json);
+std::ostream& operator<< (std::ostream& stream, const payload p) {
+    switch(p.type) {
+        case(INTEGER): {
+            int* address = static_cast<int*>(p.value);
+            stream << *address;
+            break;
+        }
+        case(DOUBLE): {
+            stream << *(static_cast<double*>(p.value));
+            break;
+        }
+        case(STRING): {
+            stream << *(static_cast<std::string*>(p.value));
+            break;
+        }
+        case(BOOLEAN): {
+            bool val = *(static_cast<bool*>(p.value));
+            stream << (val ? "true" : "false"); 
+            break;
+        }
+        case(JSON): {
+            stream << *(static_cast<json_object*>(p.value));
+            break;
+        }
+        case(LIST): {
+            auto vec = static_cast<std::vector<payload>*>(p.value);
+            stream << "[";
+            for (size_t i = 0; i < vec->size(); ++i) {
+                if (i > 0) stream << ", ";
+                stream << (*vec)[i];
+            }
+            stream << "]";
+            break;
+        }
+    }
+    return stream;
+}
+
+std::ostream& operator<< (std::ostream& stream, const json_object& json) {
+    stream << "{";
+    bool first = true;
+    for (const auto& pair : json.data) {
+        if (!first) {
+            stream << ", ";
+        }
+        stream << pair.first << ": ";
+        stream << pair.second;
+        first = false;
+    }
+    stream << "}";
+    return stream;
+}
+
+int main() {
+    using namespace std;
+    // TEST CLASS STRUCTURE
     // json_object json1, json2, json3, json4;
-    // vector<json_object> collection;
+    // vector<payload> collection;
     
     // int i = 5;
     // bool b = true;
@@ -71,8 +137,14 @@ int main() {
     // json1.addObject("second", &b);
     // string s = "example string";
     // json2.addObject("third", &s);
-    // collection.push_back(json1);
-    // collection.push_back(json2);
+
+    // payload p1, p2;
+    // p1.value = &i,
+    // p1.type = INTEGER;
+    // p2.value = &json1;
+    // p2.type = JSON;
+    // collection.push_back(p1);
+    // collection.push_back(p2);
     // json3.addObject("fourth", &collection);
     // json4.addObject("fifth", &json2);
     // cout << json1 << endl;
@@ -80,9 +152,72 @@ int main() {
     // cout << json3 << endl;
     // cout << json4 << endl;
 
+    bool testing = false;
+    string filepath = (testing) ? "/home/hapuum/cuda_learn/resource/relic_data.json" : "/home/hapuum/cuda_learn/resource/test.json";
+
+    ifstream file(filepath);
+    if (!file.is_open()) 
+    {
+        std::cerr << "Error opening file: " << filepath << std::endl;
+        return EXIT_FAILURE;
+    }
+    string json_content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    file.close();
+    cout << "File loaded successfully. Size: " << json_content.size() << " bytes" << endl;
+
     // CPU sequentially traverses json content char[] and finds pairs using stack, stores it in Task[]
     // later to be replaced with a parellel algorithm (up sweep / down sweep based prefix sum pair generation)
+    stack<task*> task_stack;
+    vector<task*> task_dispatchable;
+
+    bool inString = false;
+    bool inList = false;
+    int start = json_content.find_first_of('{');
+    task* t = new task(start, 0, new json_object());
+
+    size_t json_size = json_content.size();
+    for (int i = start + 1; i < json_size; i++) {
+        char c = json_content[i];
+        switch (c) {
+            case '{' : {  // push in current task to save it, start working on new one
+                if (!inString) {
+                    task_stack.push(t);
+                    t = new task(i, 0, new json_object());  // THIS object needs to be added to parent object too? or at least connected somehow
+                }
+                break;
+            }
+            case '}' : {  // finish and dispatch current task, go back to latest task
+                if (!inString) {
+                    t->end = i;
+                    task_dispatchable.push_back(t);                
+                    t = task_stack.top();
+                    task_stack.pop();
+                }
+                break;
+            }
+            case '"' : {
+                inString = !inString;
+                break;
+            }
+            case '[' : {
+                inList = !inList;
+                break;
+            }
+            case ']' : {
+                inList = !inList;
+                break;
+            }
+            case ',' : {
+                if (inList && !inString) {
+
+                }
+                break;
+            }
+        }
+    }    
+
+    // GPU parallel processes all of the task_dispatchable and fills in the json_object in each task
+    // extract them for host use
 
 
-    // GPU parallel processes all of the Task[] into json_object
 }
--- json_parser.h
+++ json_parser.h
@@ -11,13 +11,10 @@ namespace json {
         STRING,
         BOOLEAN,
         JSON,
-        JSON_LIST
+        LIST
     } json_type;
 
-    typedef struct {
-        void* value;
-        json_type type;
-    } payload;
+    class payload;
 
     class json_object {
         public:
@@ -30,68 +27,32 @@ namespace json {
         void addObject(std::string key, std::string* val);
         void addObject(std::string key, bool* val);
         void addObject(std::string key, json_object* val);
-        void addObject(std::string key, std::vector<json_object>* val);
+        void addObject(std::string key, std::vector<payload>* val);
 
         ~json_object();
 
         payload operator[](std::string key);
+    };
 
-        // debug purposes
-        friend std::ostream& operator<< (std::ostream& stream, const json_object& json) {
-            stream << "{";
-            bool first = true;
-            for (const auto& pair : json.data) {
-                if (!first) {
-                    stream << ", ";
-                }
-                stream << pair.first << ": ";
-                switch(pair.second.type) {
-                    case(INTEGER): {
-                        int* address = static_cast<int*>(pair.second.value);
-                        stream << *address;
-                        break;
-                    }
-                    case(DOUBLE): {
-                        stream << *(static_cast<double*>(pair.second.value));
-                        break;
-                    }
-                    case(STRING): {
-                        stream << *(static_cast<std::string*>(pair.second.value));
-                        break;
-                    }
-                    case(BOOLEAN): {
-                        bool val = *(static_cast<bool*>(pair.second.value));
-                        stream << (val ? "true" : "false"); 
-                        break;
-                    }
-                    case(JSON): {
-                        stream << *(static_cast<json_object*>(pair.second.value));
-                        break;
-                    }
-                    case(JSON_LIST): {
-                        auto vec = static_cast<std::vector<json_object>*>(pair.second.value);
-                        stream << "[";
-                        for (size_t i = 0; i < vec->size(); ++i) {
-                            if (i > 0) stream << ", ";
-                            stream << (*vec)[i];
-                        }
-                        stream << "]";
-                        break;
-                    }
-                }
-                first = false;
-            }
-            stream << "}";
-            return stream;
-        }
+    class payload {
+        public:
+        void* value;
+        json_type type;
     };
 
     class task {
         public:
         int start;
         int end;
-        json_object obj;
+        json_object* obj;
 
-        task(int s, int e, const json_object& o) : start(s), end(e), obj(o) {};
+        task(int s, int e, json_object* o) : start(s), end(e), obj(o) {};
+        ~task() {
+            delete obj;
+        }
+
+        void setStart(int s);
+        void setEnd(int e);
+        void setObject(json_object* o);
     };
 }
