bba33e522cf1aab1f8391f695bcf4c587388249f
hapuum <lbklbk9345@gmail.com>
2025-09-30 16:06:02 -0700

    merging with hsr_sim code to create good to go version. in progress.

--- json_parser.cu
+++ json_parser.cu
@@ -5,42 +5,15 @@
 
 using namespace json;
 using namespace std;
+using namespace device_utils;
 
 // implementation for json_parser.h definitions
 namespace json {
     json_object::json_object() {
-        data = std::map<std::string, payload>();
+        size = 0;
     }
 
     json_object::~json_object() {
-    }
-
-    void json_object::addObject(std::string key, int* val) {
-        data[key] = payload(val, INTEGER);
-    }
-
-    void json_object::addObject(std::string key, double* val) {
-        data[key] = payload(val, DOUBLE);
-    }
-
-    void json_object::addObject(std::string key, std::string* val) {
-        data[key] = payload(val, STRING);
-    }
-
-    void json_object::addObject(std::string key, bool* val) {
-        data[key] = payload(val, BOOLEAN);
-    }
-
-    void json_object::addObject(std::string key, json_object* val) {
-        data[key] = payload(val, JSON);
-    }
-
-    void json_object::addObject(std::string key, std::vector<payload>* val) {
-        data[key] = payload(val, LIST);
-    }
-
-    payload json_object::operator[](std::string key) {
-        return data[key];
     }
 
     void task::setStart(int s) {
@@ -51,171 +24,174 @@ namespace json {
         end = e;
     }
 
-    void task:: setObject(json_object* o) {
+    void task::setObject(json_object* o) {
         obj = o;
     }
+
     // debug purpose
-    std::ostream& operator<< (std::ostream& stream, const json_object& json);
-    std::ostream& operator<< (std::ostream& stream, const payload p) {
-        switch(p.type) {
-            case(INTEGER): {
-                int* address = static_cast<int*>(p.value);
-                stream << *address;
-                break;
-            }
-            case(DOUBLE): {
-                stream << *(static_cast<double*>(p.value));
-                break;
-            }
-            case(STRING): {
-                stream << *(static_cast<std::string*>(p.value));
-                break;
-            }
-            case(BOOLEAN): {
-                bool val = *(static_cast<bool*>(p.value));
-                stream << (val ? "true" : "false"); 
-                break;
-            }
-            case(JSON): {
-                stream << *(static_cast<json_object*>(p.value));
-                break;
-            }
-            case(LIST): {
-                auto vec = static_cast<std::vector<payload>*>(p.value);
-                stream << "[";
-                for (size_t i = 0; i < vec->size(); ++i) {
-                    if (i > 0) stream << ", ";
-                    stream << (*vec)[i];
-                }
-                stream << "]";
-                break;
-            }
-        }
-        return stream;
-    }
-    std::ostream& operator<< (std::ostream& stream, const json_object& json) {
-        stream << "{";
-        bool first = true;
-        for (const auto& pair : json.data) {
-            if (!first) {
-                stream << ", ";
-            }
-            stream << pair.first << ": ";
-            stream << pair.second;
-            first = false;
-        }
-        stream << "}";
-        return stream;
-    }
-}
+    // std::ostream& operator<< (std::ostream& stream, const json_object& json);
+    // std::ostream& operator<< (std::ostream& stream, const json_data p) {
+    //     switch(p.type) {
+    //         case(INTEGER): {
+    //             int* address = static_cast<int*>(p.value);
+    //             stream << *address;
+    //             break;
+    //         }
+    //         case(DOUBLE): {
+    //             stream << *(static_cast<double*>(p.value));
+    //             break;
+    //         }
+    //         case(STRING): {
+    //             stream << *(static_cast<std::string*>(p.value));
+    //             break;
+    //         }
+    //         case(BOOLEAN): {
+    //             bool val = *(static_cast<bool*>(p.value));
+    //             stream << (val ? "true" : "false"); 
+    //             break;
+    //         }
+    //         case(JSON): {
+    //             stream << *(static_cast<json_object*>(p.value));
+    //             break;
+    //         }
+    //         case(LIST): {
+    //             auto vec = static_cast<std::vector<json_data>*>(p.value);
+    //             stream << "[";
+    //             for (size_t i = 0; i < vec->size(); ++i) {
+    //                 if (i > 0) stream << ", ";
+    //                 stream << (*vec)[i];
+    //             }
+    //             stream << "]";
+    //             break;
+    //         }
+    //     }
+    //     return stream;
+    // }
+    // std::ostream& operator<< (std::ostream& stream, const json_object& json) {
+    //     stream << "{";
+    //     bool first = true;
+    //     for (const auto& pair : json.data) {
+    //         if (!first) {
+    //             stream << ", ";
+    //         }
+    //         stream << pair.first << ": ";
+    //         stream << pair.second;
+    //         first = false;
+    //     }
+    //     stream << "}";
+    //     return stream;
+    // }
 
-// CPU sequentially traverses json content string and finds pairs using stack, stores it in Task[]
-// replace with structural index generation (parellel gpu) -> sort -> process the structural tokens from array
-// instead of traversing entire json content string to make it more efficient 
-void create_json_tree(const std::string& json_content, std::vector<task*>& task_dispatchable) {
-    stack<task*> task_stack;
-    stack<bool> inList_stack;
-    stack<vector<payload>*> current_list_stack;
-
-    bool inString = false;
-    bool inList = false;
-    int start = json_content.find_first_of('{');
-    json_object* current_json =  new json_object();
-    task* current_task = new task(start, 0, current_json);
-    string current_string;
-    vector<payload>* current_list;
-
-    size_t json_size = json_content.size();
-    for (int i = start; i < json_size; i++) {
-        char c = json_content[i];
-        if (inString && c != '"') current_string = current_string + c;
-        switch (c) {
-            case '{' : {  // push in current task to save it, start working on new one
-                if (inString) break;
-                task_stack.push(current_task);
-                json_object* new_json = new json_object();
-                if (!inList) {
-                    current_json->addObject(current_string, new_json);
-
-                }
-                else {  // list stores payloads, instead of pair<string,payload>
-                    payload p = payload(new_json, JSON);
-                    current_list->push_back(p);
-                }
-                current_json = new_json;
-                current_task = new task(i, 0, current_json);
+    void addObject(json_object& json, string_buffer& strbuf, json_buffer& jsonbuf, list_buffer& listbuf, const json_data data) {
+        if (json.size >= 16) return;
+        json_type t = data.type;
+        json.child_json[json.size].type = t;
+        json_value v;
+        switch(t) {
+            case(STRING):
+                v.string_buffer_index = strbuf.size;
+                json.child_json[json.size].val = v;
+                strbuf.size++;
                 break;
-            }
-            case '}' : {  // finish and dispatch current task, go back to latest task
-                if (inString) break;
-                current_task->end = i;
-                task_dispatchable.push_back(current_task);
-                current_task = task_stack.top();
-                current_json = current_task->obj;
-                task_stack.pop();
+            case(JSON):
+                v.json_buffer_index = jsonbuf.size;
+                json.child_json[json.size].val = v;
+                jsonbuf.size++;
                 break;
-            }
-            case '"' : {
-                inString = !inString;
-                if (inString) current_string = "";  // entering new string
+            case(LIST):
+                v.list_buffer_index = listbuf.size;
+                json.child_json[json.size].val = v;
+                listbuf.size++;
                 break;
-            }
-            case '[' : {  // need better logic when adding nested array support
-                inList_stack.push(inList);
-                inList = true;
-                current_list_stack.push(current_list);
-                current_list = new vector<payload>();
-                current_json->addObject(current_string, current_list);
-                cout << current_string << endl;
-                break;
-            }
-            case ']' : {
-                inList = inList_stack.top();
-                inList_stack.pop();
-                current_list = current_list_stack.top();
-                current_list_stack.pop();
-                break;
-            }
             default:
                 break;
         }
-    }  
+        strbuf.size++; // for key
+        json.size++; // added an element
+    }
 }
 
-// debug purpose and reference code for how to create / access different classes
-void test_class_structure() {
-    json_object json1, json2, json3, json4;
-    vector<payload> collection;
-    
-    int i = 5;
-    bool b = true;
-    json1.addObject("first", &i);
-    json1.addObject("second", &b);
-    string s = "example string";
-    json2.addObject("third", &s);
+namespace device_utils {
 
-    payload p1(&i, INTEGER);
-    payload p2(&json1, JSON);
-    collection.push_back(p1);
-    collection.push_back(p2);
-    json3.addObject("fourth", &collection);
-    json4.addObject("fifth", &json2);
-    cout << json1 << endl;
-    cout << json2 << endl;
-    cout << json3 << endl;
-    cout << json4 << endl;
 }
 
-__global__ void parse_json() {
-    __shared__ extern char json_content[];
+__global__ void get_sorted_tokens(const std::string& json_content, device_vector<StructuralToken> tokens) {
 
-    int tid = threadIdx.x + blockDim.x * blockIdx.x;
-    
 }
+
+
+
+// void create_json_tree(const std::string& json_content, std::vector<task*>& task_dispatchable) {
+//     stack<task*> task_stack;
+//     stack<bool> inList_stack;
+//     stack<vector<json_data>*> current_list_stack;
+//     stack<int> gpu_index_stack;
+//     bool inString = false;
+//     bool inList = false;
+//     int start = json_content.find_first_of('{');
+//     json_object* current_json =  new json_object();
+//     int current_gpu_index = 0;
+//     task* current_task = new task(start, 0, current_json);
+//     string current_string;
+//     vector<json_data>* current_list;
+//     size_t json_size = json_content.size();
+//     for (int i = start; i < json_size; i++) {
+//         char c = json_content[i];
+//         if (inString && c != '"') current_string = current_string + c;
+//         switch (c) {
+//             case '{' : {  // push in current task to save it, start working on new one
+//                 if (inString) break;
+//                 task_stack.push(current_task);
+//                 json_object* new_json = new json_object();
+//                 if (!inList) {
+//                     current_json->addObject(current_string, new_json);
+//                 }
+//                 else {  // list stores payloads, instead of pair<string,payload>
+//                     json_data p = json_data(new_json, JSON);
+//                     current_list->push_back(p);
+//                 }
+//                 current_json = new_json;
+//                 current_task = new task(i, 0, current_json);
+//                 break;
+//             }
+//             case '}' : {  // finish and dispatch current task, go back to latest task
+//                 if (inString) break;
+//                 current_task->end = i;
+//                 task_dispatchable.push_back(current_task);
+//                 current_task = task_stack.top();
+//                 current_json = current_task->obj;
+//                 task_stack.pop();
+//                 break;
+//             }
+//             case '"' : {
+//                 inString = !inString;
+//                 if (inString) current_string = "";  // entering new string
+//                 break;
+//             }
+//             case '[' : {
+//                 inList_stack.push(inList);
+//                 inList = true;
+//                 current_list_stack.push(current_list);
+//                 current_list = new vector<json_data>();
+//                 current_json->addObject(current_string, current_list);
+//                 cout << current_string << endl;
+//                 break;
+//             }
+//             case ']' : {
+//                 inList = inList_stack.top();
+//                 inList_stack.pop();
+//                 current_list = current_list_stack.top();
+//                 current_list_stack.pop();
+//                 break;
+//             }
+//             default:
+//                 break;
+//         }
+//     }  
+// }
+
 
 int main() {
-    //test_class_structure();
-
     bool testing = false;
     string filepath = (!testing) ? "/home/hapuum/cuda_learn/resource/relic_data.json" : "/home/hapuum/cuda_learn/resource/test.json";
 
@@ -228,20 +204,10 @@ int main() {
     string json_content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
     file.close();
     cout << "File loaded successfully. Size: " << json_content.size() << " bytes" << endl;
-    
-    vector<task*> task_dispatchable;
-    create_json_tree(json_content, task_dispatchable);
-
-    task* d_task_array;
-    cudaMalloc((void**) d_task_array, task_dispatchable.size() * sizeof(task*));
-    // debug + prep for gpu
-    for (int i = 0; i < task_dispatchable.size(); i++) {
-        task* t = task_dispatchable[i];
-        cout << "start :" << t->start << ", end:" << t->end << " json: " << *(t->obj) << " at memory: " << t->obj << endl;
-    }
 
 
     // now remap task_dispatchable to nice array structure where each thread can access
     // might need to implement device_compatible_vector and device_compatible_stack for the actual kernel code
-   
+    
 }
+ 
--- json_parser.h
+++ json_parser.h
@@ -3,6 +3,11 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#define MAX_CHILDREN_PER_JSON 16
+#define MAX_STRINGS 16384
+#define MAX_STRING_LENGTH 32
+#define MAX_LISTS   16384
+#define MAX_LIST_LENGTH 65536
 
 namespace json {
     typedef enum {
@@ -14,40 +19,66 @@ namespace json {
         LIST
     } json_type;
 
-    class payload;
+    typedef union {
+        int i;
+        double d;
+        int string_buffer_index;
+        bool b;
+        int list_buffer_index;
+        int json_buffer_index; 
+    } json_value;
+
+    class json_data;
+    class string_buffer;
+    class json_buffer;
+    class list_buffer;
 
     class json_object {
         public:
-        std::map<std::string, payload> data;
-        
-        json_object();
-
-        void addObject(std::string key, int* val);
-        void addObject(std::string key, double* val);
-        void addObject(std::string key, std::string* val);
-        void addObject(std::string key, bool* val);
-        void addObject(std::string key, json_object* val);
-        void addObject(std::string key, std::vector<payload>* val);
-
-        ~json_object();
-
-        payload operator[](std::string key);
+        int key_string_indices[16];
+        json_data child_json[16];
+        int size;
     };
 
-    class payload {
+    class json_data {
         public:
-        void* value;
+        json_value val;
         json_type type;
-        payload() : value(nullptr), type(INTEGER) { }
-        payload(void* val, json_type t) : value(val), type(t) {};
     };
+
+    // device compatible string buffer. index to this serves like a pointer.
+    class string_buffer {
+        public:
+        char data[MAX_STRINGS][MAX_STRING_LENGTH]; 
+        int size;
+        // string lengths are kind of hard to determine right off of tokens and does not vary much so 2D array makes sense
+    };
+    class json_buffer {
+        public:
+        json_object data[MAX_CHILDREN_PER_JSON];
+        int size;
+    };
+    class list_buffer {
+        public:
+        json_object data[MAX_LIST_LENGTH];
+        int list_starting_index[MAX_LISTS];
+        int size;
+        // Tokens are perfectly fine to generate starting index of each list,
+        // where other objects navigate into list_starting_index then use that to get where to start data
+    };
+
+    // adds an object to json and modifies the global buffers as needed.
+    // string buffer size will increase in all operations, and json's key will be set to new strbuf size
+    // the string buffer may additionally increase when datatype is string
+    // similar logic for jsonbuf / listbuf. increase its size and point to correct location as needed.
+    void addObject(json_object& json, string_buffer& strbuf, json_buffer& jsonbuf, list_buffer& listbuf, const json_data data);
 
     class task {
         public:
         int start;
         int end;
-        json_object* obj;
-
+        json_object* obj; 
+        
         task(int s, int e, json_object* o) : start(s), end(e), obj(o) {};
         ~task() {
             delete obj;
@@ -56,5 +87,71 @@ namespace json {
         void setStart(int s);
         void setEnd(int e);
         void setObject(json_object* o);
+        void setGPUIndex(int g);
+    };
+
+    typedef enum {
+        OPEN_BRACE,
+        CLOSE_BRACE,
+        OPEN_BRACKET,
+        CLOSE_BRACKET,
+        COLON,
+        COMMA
+    } tokenType;
+
+    class StructuralToken {
+        int location;
+        tokenType t;
+    };
+}
+
+namespace device_utils{
+    template<typename T> class device_vector {
+        T* data;
+        size_t capacity, length;
+        __device__ DeviceVector : data(nullptr), capacity(16), length(0) {
+            data = new T[capacity];
+        }
+
+        __device__ DeviceVector(size_t c) : data(nullptr), capacity(c), length(0) {
+            data = new T[capacity];
+        }
+
+        __device__ ~DeviceVector() { delete[] data; }
+
+        __device__ void push_back(const T& value) {
+            if (length >= capacity) {
+                capacity *= 2;
+                T* new_data = new T[capacity];
+                for (size_t i = 0; i < length; ++i) new_data[i] = data[i];
+                delete[] data;
+                data = new_data;
+            }
+            data[length++] = value;
+        }
+
+        __device__ void set(size_t idx, const T& value) { data[idx] = value; }
+
+        __device__ void join(const DeviceVector<T>* next) {
+            for (int i = 0; i < next->length; i++) {
+                this->push_back(next->get(i));
+            }
+        }
+
+        __device__ T* getInternalArray() {
+            return data;
+        }
+
+        __host__ __device__ void printVector() {
+            for (int i = 0; i < this->length; i++) {
+                printf("%d \n", this->get(i));
+            }
+            printf("\n");
+        }
+
+        // host can only read and cannot modify.
+        __host__ __device__ T& operator[](size_t idx) { return data[idx]; }
+        __host__ __device__ size_t size() const { return length; }
+        __host__ __device__ T& get(size_t idx) { return data[idx]; }
     };
 }
